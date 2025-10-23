e92142bf64115eeee5e06dccda84d4bef4f98622
0xgleb <gleb.dianov@protonmail.com>
2025-10-22 21:35:09 -0300

    upgrade setup

--- flake.nix
+++ flake.nix
@@ -1,39 +1,80 @@
 {
   inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
+    devenv.url = "github:cachix/devenv/v1.7";
+    devenv.inputs = {
+      nixpkgs.follows = "nixpkgs";
+      git-hooks.follows = "git-hooks";
+    };
   };
 
-  nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  outputs = { self, nixpkgs, flake-utils, git-hooks, devenv, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        hooks = {
+          # Nix
+          nil.enable = true;
+          nixfmt-classic.enable = true;
+
+          # Haskell
+          fourmolu.enable = true;
+          hlint.enable = true;
+
+          # TypeScript
+          eslint.enable = true;
+          prettier.enable = true;
+        };
+
+        devShell = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [{
+            # https://devenv.sh/reference/options/
+            packages = with pkgs.haskellPackages; [
+              haskell-language-server
+              fourmolu
+              hlint
+            ];
+
+            languages = {
+              nix.enable = true;
+              haskell.enable = true;
+              javascript.enable = true;
+              javascript.pnpm.enable = true;
+              typescript.enable = true;
+            };
+
+            env = { };
+            git-hooks = { inherit hooks; };
+            difftastic.enable = true;
+            cachix.enable = true;
+          }];
+        };
+
+      in {
+        devShells.default = devShell;
+
+        checks.git-hooks = git-hooks.lib.${system}.run {
+          inherit hooks;
+          src = self;
+        };
+
+        packages = {
+          devenv-up = devShell.config.procfileScript;
+          default = devShell.config.procfileScript;
+        };
       });
 
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-              packages = with pkgs.haskellPackages; [
-                haskell-language-server
-                fourmolu
-                hlint
-              ];
-              languages.haskell.enable = true;
-            }];
-          };
-        });
-    };
+  nixConfig = {
+    extra-substituters = "https://devenv.cachix.org";
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    allow-unfree = true;
+  };
 }
